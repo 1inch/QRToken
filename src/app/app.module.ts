e7b89a2a07da5cc98a224d3be091e5acdcda9297
deacix <info@deacix.de>
2019-03-09 02:45:59 +0100

    added solidity dev libs and tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {BaseComponent} from './base/base.component';
 import {NoContentComponent} from './no-content/no-content.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {Web3Service} from './util/web3.service';
+import {FormsModule} from '@angular/forms';
 
 export const routes: Routes = [
   {
@@ -31,6 +32,7 @@ export const routes: Routes = [
     CommonModule,
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     FontAwesomeModule,
     RouterModule.forRoot(
       routes,
