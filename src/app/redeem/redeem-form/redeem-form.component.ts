4c8919d4102b64780e48e066df9ede0da8a12ead
deacix <info@deacix.de>
2019-03-10 09:12:10 +0100

    fix

--- src/app/redeem/redeem-form/redeem-form.component.ts
+++ src/app/redeem/redeem-form/redeem-form.component.ts
@@ -126,7 +126,7 @@ export class RedeemFormComponent implements OnInit {
 
                     pairs.subscribe(d => {
                         const lastPrice = d['ETH_' + this.token.symbol]['lastPrice'];
-                        this.fee = lastPrice * 300000 * 20e9 / 10 ** d['ETH_' + this.token.symbol]['decimals'];
+                        this.fee = 300000 * 20e9 / lastPrice / (10 ** d['ETH_' + this.token.symbol]['decimals']);
 
                         console.log('Fees', this.fee);
 
