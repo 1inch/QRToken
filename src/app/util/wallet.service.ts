a9f17a04575bbc58db2740cabf1c8a0bf5495223
deacix <info@deacix.de>
2019-03-16 00:39:15 +0100

    fix

--- src/app/util/wallet.service.ts
+++ src/app/util/wallet.service.ts
@@ -211,13 +211,17 @@ export class WalletService implements OnInit {
             this.web3Service.web3.utils.keccak256(
                 this.web3Service.web3.utils.padLeft(receiver, 40)
                     .concat(this.web3Service.web3.utils.padLeft(this.web3Service.web3.utils.toHex(feePrecent), 64).substr(2))
-                    .concat(this.web3Service.web3.utils.padLeft(fromAddress, 40))
+                    .concat(this.web3Service.web3.utils.padLeft(fromAddress, 40).substr(2))
                 , {encoding: 'hex'}
             )
         );
 
-        console.log('Message', this.web3Service.web3.utils.padLeft(receiver, 40)
-            .concat(this.web3Service.web3.utils.padLeft(this.web3Service.web3.utils.toHex(feePrecent), 64).substr(2)));
+        console.log('receiver', receiver);
+        console.log('fromAddress', fromAddress);
+
+        console.log('Message',  this.web3Service.web3.utils.padLeft(receiver, 40)
+            .concat(this.web3Service.web3.utils.padLeft(this.web3Service.web3.utils.toHex(feePrecent), 64).substr(2))
+            .concat(this.web3Service.web3.utils.padLeft(fromAddress, 40).substr(2)));
 
 
         console.log('Message Keccak', this.web3Service.web3.utils.keccak256(this.web3Service.web3.utils.padLeft(receiver, 40)
